8a7da1e827d82e4957633619aecac3b80fa090d3
bosung park <81157873+bosungpark@users.noreply.github.com>
2023-04-23 21:09:50 +0900

    bind param

--- src/service_layer/services.py
+++ src/service_layer/services.py
@@ -1,3 +1,5 @@
+from sqlalchemy import text, bindparam
+
 from src.adapters.repository import tweet_repository
 from src.domain.models import Tweet
 
@@ -10,9 +12,12 @@ def publish_timeline(user_id: int):
 
 def subscribe_timeline(user_id):
     tweets = tweet_repository._session.execute(
-            f"SELECT tweets.*, users.* FROM tweets "  # type: ignore
-            f"JOIN users ON tweets.sender_id = users.id) "  
-            f"JOIN follows ON follows.followee_id = users.id "
-            f"WHERE follows.follower_id = {user_id}"
+            text("""  
+            SELECT tweets.*, users.* FROM tweets
+            JOIN users ON tweets.sender_id = users.id) 
+            JOIN follows ON follows.followee_id = users.id
+            WHERE follows.follower_id = :user_id
+            """).bindparams(bindparam('user_id')),
+        params={'user_id':user_id}
         )
     return tweets
